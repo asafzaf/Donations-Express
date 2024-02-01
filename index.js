669f3617e9c75f0139c9b5f48057d093cac8f162
asafzaf <asaf.zafrir@gmail.com>
2024-02-01 18:03:06 +0200

    added general 404 not found result.

--- index.js
+++ index.js
@@ -1,26 +1,39 @@
-require('dotenv').config();
-const express = require('express');
-const logger = require('morgan');
-const { donationsRouter } = require('./routers/donations.router');
-const { connnectDB } = require('./db/donations.db');
+require("dotenv").config();
+const express = require("express");
+const logger = require("morgan");
+const { donationsRouter } = require("./routers/donations.router");
+const { connnectDB } = require("./db/donations.db");
+const { ServerError, NotFoundError, BadRequestError } = require("./errors/errors");
+
 
 const app = express();
 const port = process.env.PORT || 8080;
 
 module.exports = app;
 
-connnectDB();   
+connnectDB();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(logger('dev'));
+app.use(logger("dev"));
 
-app.use('/api/donations', donationsRouter);
+app.use("/api/donations", donationsRouter);
 
-app.all('*', (req, res, next) => {
-    res.status(404).send('Not Found');
+app.all("*", (req, res, next) => {
+  const error = new NotFoundError(req.originalUrl);
+  next(error);
+});
+
+app.use((err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.name = err.name || "error";
+
+  res.status(err.statusCode).json({
+    name: err.name,
+    message: err.message,
+  });
 });
 
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+  console.log(`Server is running on port ${port}`);
 });
