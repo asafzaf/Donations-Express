ea78c214ee6d97a323e589e7359450249d1e42fd
asafzaf <asaf.zafrir@gmail.com>
2024-02-08 14:37:54 +0200

    added NODE_ENV value.

--- constants.js
+++ constants.js
@@ -1,7 +1,8 @@
+/* eslint-disable no-undef */
 module.exports = {
-    DB_USER: process.env.DB_USER,
-    DB_PASS: process.env.DB_PASS,
-    DB_HOST: process.env.DB_HOST,
-    DB_NAME: process.env.DB_NAME,
-    NODE_ENV: process.env.NODE_ENV
-}
+  DB_USER: process.env.DB_USER,
+  DB_PASS: process.env.DB_PASS,
+  DB_HOST: process.env.DB_HOST,
+  DB_NAME: process.env.DB_NAME,
+  NODE_ENV: process.env.NODE_ENV,
+};
