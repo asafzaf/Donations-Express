af778dce1ecccb96eca7d44c809e9a3c2171ba47
asafzaf <asaf.zafrir@gmail.com>
2024-02-08 12:25:44 +0200

    erase error catcher from end of route block.

--- routers/donations.router.js
+++ routers/donations.router.js
@@ -1,6 +1,6 @@
 const { Router } = require("express");
 const donationsController = require("../controllers/donations.controller");
-const { ServerError, NotFoundError, BadRequestError } = require("../errors/errors");
+const { NotFoundError } = require("../errors/errors");
 const donationsRouter = new Router();
 
 const donationsControllerInstance = new donationsController();
@@ -11,18 +11,11 @@ donationsRouter
   .post("/items", donationsControllerInstance.createDonation)
   .put("/items/:id", donationsControllerInstance.putDonation)
   .delete("/items/:id", donationsControllerInstance.deleteDonation)
-  .all("*", (req, res, next) => {
+  .all("*", (req, res) => {
     const error = new NotFoundError(req.originalUrl);
-    next(error);
-  });
-
-  donationsRouter.use((err, req, res, next) => {
-    err.statusCode = err.statusCode || 500;
-    err.name = err.name || "error";
-  
-    res.status(err.statusCode).json({
-      name: err.name,
-      message: err.message,
+    res.status(error.statusCode).json({
+      name: error.name,
+      message: error.message,
     });
   });
 
