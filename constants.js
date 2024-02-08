992bc5e2f3ddaa8b24c55948e3090faa459d9ba5
asafzaf <asaf.zafrir@gmail.com>
2024-02-08 13:22:02 +0200

    added NODE_ENV.

--- constants.js
+++ constants.js
@@ -2,5 +2,6 @@ module.exports = {
     DB_USER: process.env.DB_USER,
     DB_PASS: process.env.DB_PASS,
     DB_HOST: process.env.DB_HOST,
-    DB_NAME: process.env.DB_NAME
+    DB_NAME: process.env.DB_NAME,
+    NODE_ENV: process.env.NODE_ENV
 }
