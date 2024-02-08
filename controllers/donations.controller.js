248f24cb750b6d1717de5de3e0b5e3bdd5d56422
asafzaf <asaf.zafrir@gmail.com>
2024-02-08 14:46:23 +0200

    split check of params to outsider function that throws errors.
    change importation of repository from class & instance to function handler.

--- controllers/donations.controller.js
+++ controllers/donations.controller.js
@@ -1,24 +1,23 @@
-const { Donation } = require("../models/donations.model");
 const EventEmmiter = require("events");
 const {
   ServerError,
   NotFoundError,
   BadRequestError,
 } = require("../errors/errors");
-const DonationsRepository = require("../repositories/donations.repository");
-
-const donationsRepository = new DonationsRepository();
+const donationsRepository = require("../repositories/donations.repository");
 
 module.exports = class donationsController extends EventEmmiter {
   constructor() {
     super();
-    this.hello = "hello world";
   }
 
   getAllDonations = async (req, res) => {
     try {
       const donations = await donationsRepository.find();
-      res.status(200).json({
+      if (!donations || donations.length === 0) {
+        throw new NotFoundError("Donations");
+      }
+      return res.status(200).json({
         message: "Donations retrieved successfully",
         status: "success",
         results: donations.length,
@@ -27,29 +26,24 @@ module.exports = class donationsController extends EventEmmiter {
         },
       });
     } catch (error) {
-      const err = new ServerError("getAll");
-      res.status(err.statusCode).json({
-        name: err.name,
-        message: err.message,
+      res.status(error?.statusCode || 500).json({
+        name: error.name,
+        message: error.message,
       });
     }
   };
 
   getDonation = async (req, res) => {
+    const { id } = req.params;
     try {
-      if (
-        req.params.id === null ||
-        req.params.id === undefined ||
-        req.params.id === "" ||
-        req.params.id === " "
-      ) {
+      if (id === null || id === undefined || id === "" || id === " ") {
         throw new BadRequestError("id");
       }
       const donation = await donationsRepository.retrieve(req.params.id);
-      if (!donation) {
-        throw new NotFoundError("Donation");
+      if (!donation || Object.keys(donation).length === 0) {
+        throw new NotFoundError(`Donation with id ${id}`);
       }
-      res.status(200).json({
+      return res.status(200).json({
         message: "Donation retrieved successfully",
         status: "success",
         data: {
@@ -57,12 +51,7 @@ module.exports = class donationsController extends EventEmmiter {
         },
       });
     } catch (error) {
-      if (error.name === "TypeError") {
-        error = new BadRequestError("id");
-      } else {
-        error = new ServerError("getOne");
-      }
-      res.status(error.statusCode).json({
+      res.status(error?.statusCode || 500).json({
         name: error.name,
         message: error.message,
       });
@@ -71,8 +60,12 @@ module.exports = class donationsController extends EventEmmiter {
 
   createDonation = async (req, res) => {
     try {
+      checkDonationsParams(req.body);
       const newDonation = await donationsRepository.create(req.body);
-      res.status(201).json({
+      if (!newDonation) {
+        throw new ServerError("post");
+      }
+      return res.status(201).json({
         message: "Donation created successfully",
         status: "success",
         data: {
@@ -80,16 +73,17 @@ module.exports = class donationsController extends EventEmmiter {
         },
       });
     } catch (error) {
-      const err = new BadRequestError("params");
-      res.status(err.statusCode).json({
-        name: err.name,
-        message: err.message,
+      res.status(error?.statusCode || 500).json({
+        name: error.name,
+        message: error.message,
       });
     }
   };
 
   putDonation = async (req, res) => {
     try {
+      checkDonationsParams(req.body);
+      checkId(req.params.id);
       const donation = await donationsRepository.put(req.params.id, req.body, {
         new: true,
         runValidators: true,
@@ -97,7 +91,7 @@ module.exports = class donationsController extends EventEmmiter {
       if (!donation) {
         throw new NotFoundError("Donation");
       }
-      res.status(200).json({
+      return res.status(200).json({
         message: "Donation updated successfully",
         status: "success",
         data: {
@@ -105,12 +99,7 @@ module.exports = class donationsController extends EventEmmiter {
         },
       });
     } catch (error) {
-      if (error.name === "CastError") {
-        error = new BadRequestError("id");
-      } else {
-        error = new ServerError("put");
-      }
-      res.status(error.statusCode).json({
+      res.status(error?.statusCode || 500).json({
         name: error.name,
         message: error.message,
       });
@@ -119,33 +108,54 @@ module.exports = class donationsController extends EventEmmiter {
 
   deleteDonation = async (req, res) => {
     try {
-      if (
-        req.params.id === null ||
-        req.params.id === undefined ||
-        req.params.id === "" ||
-        req.params.id === " "
-      ) {
-
-        throw new BadRequestError("id");
-      }
-
+      checkId(req.params.id);
       const donation = await donationsRepository.delete(req.params.id);
       if (!donation) {
         throw new NotFoundError("Donation");
       }
-      res.status(204).json({
+      return res.status(204).json({
         message: "Donation deleted successfully",
         status: "success",
         data: null,
       });
     } catch (error) {
-      if (error.name === "CastError") {
-        error = new BadRequestError("id");
-      }
-      res.status(error.statusCode).json({
+      res.status(error?.statusCode || 500).json({
         name: error.name,
         message: error.message,
       });
     }
   };
+};
+
+const checkId = (id) => {
+  if (id === null || id === undefined || id === "" || id === " ") {
+    throw new BadRequestError("id");
+  }
+};
+
+const checkDonationsParams = (body) => {
+  if (body === null || body === undefined || Object.keys(body).length === 0) {
+    throw new BadRequestError("params");
+  } else if (
+    body.name === null ||
+    body.name === undefined ||
+    body.name === "" ||
+    body.name === " "
+  ) {
+    throw new BadRequestError("name");
+  } else if (
+    body.email === null ||
+    body.email === undefined ||
+    body.email === "" ||
+    body.email === " "
+  ) {
+    throw new BadRequestError("email");
+  } else if (
+    body.amount === null ||
+    body.amount === undefined ||
+    body.amount === "" ||
+    body.amount === " "
+  ) {
+    throw new BadRequestError("amount");
+  }
 };
